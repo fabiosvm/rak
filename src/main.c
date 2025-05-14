5fb5fb568aa7f61f50c51a42223e23611a72cbf4
fabiosvm <fabiosvm@outlook.com>
2025-05-14 15:08:01 -0300

    refactor: restructure main.c for better readability and error management

--- src/main.c
+++ src/main.c
@@ -17,12 +17,12 @@
 static void shutdown(int sig);
 static bool has_opt(int argc, const char *argv[], const char *opt);
 static const char *get_arg(int argc, const char *argv[], int idx);
-static void check_error(RakError *err);
-static RakString *read_from_stdin(void);
+static RakString *read_from_stdin(RakError *err);
 static RakString *read_from_file(const char *path, RakError *err);
 static FILE *open_file(const char *path, RakError *err);
 static int file_size(FILE *fp);
-static void run(RakVM *vm, RakFunction *fn, RakError *err);
+static RakFunction *compile_from_stdin(RakError *err);
+static RakFunction *compile_from_file(const char *path, RakError *err);
 
 static void shutdown(int sig)
 {
@@ -51,38 +51,39 @@ static const char *get_arg(int argc, const char *argv[], int idx)
   return NULL;
 }
 
-static void check_error(RakError *err)
+static RakString *read_from_stdin(RakError *err)
 {
-  if (rak_is_ok(err)) return;
-  rak_error_print(err);
-  exit(EXIT_FAILURE);
-}
-
-static RakString *read_from_stdin(void)
-{
-  RakError err;
-  rak_error_init(&err);
-  RakString *source = rak_string_new(&err);
-  check_error(&err);
+  rak_error_init(err);
+  RakString *source = rak_string_new(err);
+  if (!rak_is_ok(err)) return NULL;
   char c = (char) fgetc(stdin);
   while (c != EOF)
   {
-    rak_string_inplace_append_cstr(source, 1, &c, &err);
-    check_error(&err);
+    rak_string_inplace_append_cstr(source, 1, &c, err);
+    if (!rak_is_ok(err))
+    {
+      rak_string_free(source);
+      return NULL;
+    }
     c = (char) fgetc(stdin);
   }
-  rak_string_inplace_append_cstr(source, 1, "\0", &err);
-  check_error(&err);
-  return source;
+  rak_string_inplace_append_cstr(source, 1, "\0", err);
+  if (rak_is_ok(err)) return source;
+  rak_string_free(source);
+  return NULL;
 }
 
 static RakString *read_from_file(const char *path, RakError *err)
 {
   FILE *fp = open_file(path, err);
-  check_error(err);
+  if (!rak_is_ok(err)) return NULL;
   int len = file_size(fp);
   RakString *source = rak_string_new_with_capacity(len, err);
-  check_error(err);
+  if (!rak_is_ok(err))
+  {
+    fclose(fp);
+    return NULL;
+  }
   int _len = (int) fread(rak_string_chars(source), 1, len, fp);
   if (_len != len)
   {
@@ -91,11 +92,12 @@ static RakString *read_from_file(const char *path, RakError *err)
     rak_string_free(source);
     return NULL;
   }
+  fclose(fp);
   source->slice.len = len;
   rak_string_inplace_append_cstr(source, 1, "\0", err);
-  check_error(err);
-  fclose(fp);
-  return source;
+  if (rak_is_ok(err)) return source;
+  rak_string_free(source);
+  return NULL;
 }
 
 static FILE *open_file(const char *path, RakError *err)
@@ -119,14 +121,30 @@ static int file_size(FILE *fp)
   return size;
 }
 
-static void run(RakVM *vm, RakFunction *fn, RakError *err)
+static RakFunction *compile_from_stdin(RakError *err)
 {
-  rak_vm_init(vm, RAK_VM_VSTK_DEFAULT_SIZE, RAK_VM_CSTK_DEFAULT_SIZE, err);
-  check_error(err);
-  rak_builtin_load_globals(vm, err);
-  check_error(err);
-  rak_vm_run(vm, fn, err);
-  check_error(err);
+  RakString *file = rak_string_new_from_cstr(-1, "<stdin>", err);
+  if (!rak_is_ok(err)) return NULL;
+  RakString *source = read_from_stdin(err);
+  if (!rak_is_ok(err))
+  {
+    rak_string_free(file);
+    return NULL;
+  }
+  return rak_compile(file, source, err);
+}
+
+static RakFunction *compile_from_file(const char *path, RakError *err)
+{
+  RakString *file = rak_string_new_from_cstr(-1, path, err);
+  if (!rak_is_ok(err)) return NULL;
+  RakString *source = read_from_file(path, err);
+  if (!rak_is_ok(err))
+  {
+    rak_string_free(file);
+    return NULL;
+  }
+  return rak_compile(file, source, err);
 }
 
 int main(int argc, const char *argv[])
@@ -134,32 +152,46 @@ int main(int argc, const char *argv[])
   signal(SIGINT, shutdown);
   RakError err;
   rak_error_init(&err);
-  RakString *file;
-  RakString *source;
   const char *path = get_arg(argc, argv, 0);
+  RakFunction *fn;
   if (path)
-  {
-    file = rak_string_new_from_cstr(-1, path, &err);
-    check_error(&err);
-    source = read_from_file(path, &err);
-    check_error(&err);
-  }
+    fn = compile_from_file(path, &err);
   else
+    fn = compile_from_stdin(&err);
+  if (!rak_is_ok(&err))
   {
-    file = rak_string_new_from_cstr(-1, "<stdin>", &err);
-    check_error(&err);
-    source = read_from_stdin();
+    rak_error_print(&err);
+    return EXIT_FAILURE;
   }
-  RakFunction *fn = rak_compile(file, source, &err);
-  check_error(&err);
   if (has_opt(argc, argv, "-c"))
   {
     rak_dump_function(fn);
+    rak_function_free(fn);
     return EXIT_SUCCESS;
   }
   RakVM vm;
-  run(&vm, fn, &err);
-  check_error(&err);
+  rak_vm_init(&vm, RAK_VM_VSTK_DEFAULT_SIZE, RAK_VM_CSTK_DEFAULT_SIZE, &err);
+  if (!rak_is_ok(&err))
+  {
+    rak_error_print(&err);
+    rak_function_free(fn);
+    return EXIT_FAILURE;
+  }
+  rak_builtin_load_globals(&vm, &err);
+  if (!rak_is_ok(&err))
+  {
+    rak_error_print(&err);
+    rak_function_free(fn);
+    rak_vm_deinit(&vm);
+    return EXIT_FAILURE;
+  }
+  rak_vm_run(&vm, fn, &err);
+  if (!rak_is_ok(&err))
+  {
+    rak_error_print(&err);
+    rak_vm_deinit(&vm);
+    return EXIT_FAILURE;
+  }
   rak_vm_deinit(&vm);
   return EXIT_SUCCESS;
 }
