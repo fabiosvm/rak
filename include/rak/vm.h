997c0eb97591bfff45e292d806eda7c55ee78e31
deleterium <rui.santana@protonmail.com>
2025-05-21 21:25:09 -0300

    Note for known bug.

--- include/rak/vm.h
+++ include/rak/vm.h
@@ -755,6 +755,7 @@ static inline void rak_vm_unpack_fields(RakFiber *fiber, uint8_t n, RakError *er
     {
       rak_error_set(err, "record has no field named '%.*s'",
         rak_string_len(name), rak_string_chars(name));
+      // Fix: double free if error is not the first field in destructuring.
       return;
     }
     RakValue _val = rak_record_get(rec, idx).val;
